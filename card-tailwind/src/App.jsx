0fe458e0842f7b19602b7e5060cfccd3231833fa
NunzioBasciano <nbasho1988@gmail.com>
2024-07-13 19:48:32 +0200

    feat: layout and classes  with tailwind

--- card-tailwind/src/App.jsx
+++ card-tailwind/src/App.jsx
@@ -1,8 +1,35 @@
+import IconEthereum from './components/icons/IconEthereum'
+import IconClock from './components/icons/IconClock'
+
+
 function App() {
 
   return (
     <>
-      <h1>Hello tailwind</h1>
+      <div className="bg-regal-blue flex flex-col gap-4 p-5 rounded-lg shadow-custom w-80">
+        <div className='mx-auto rounded-lg'>
+          <img className='object-cover rounded-lg h-full w-full' src="../public/images/image-equilibrium.jpg" alt="" />
+        </div>
+        <h2 className='font-semibold text-white text-2xl'>Equilibrium #3429</h2>
+        <p className='text-gray'>Our Equilibrium collection promotes balance and calm.</p >
+        <div className='flex justify-between'>
+          <span className='items-center flex gap-2 '>
+            <IconEthereum />
+            <h3 className='font-semibold text-light-green'>0.041 ETH</h3>
+          </span>
+          <span className='items-center flex gap-2'>
+            <IconClock />
+            <h3 className='font-semibold text-gray'>3 days left</h3>
+          </span>
+        </div>
+        <div className='bg-gray h-px w-full'></div>
+        <span className='flex items-center mb-0.5'>
+          <img className='border border-gray rounded-full w-7' src="../public/images/image-avatar.png" alt="" />
+          <h5 className='mx-2  text-gray'>Creation of</h5>
+          <h4 className='text-white'>Jules Wyvern</h4>
+        </span>
+
+      </div>
     </>
   )
 }
--- card-tailwind/tailwind.config.js
+++ card-tailwind/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': 'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px',
+      },
+      colors: {
+        'regal-blue': '#15253f',
+        'dark-blue': '#09182b',
+        'light-green': '#1ed6d5',
+        'gray': '#91abd2',
+      },
+    },
   },
   plugins: [],
 }
